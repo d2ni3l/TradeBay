2b361a6666e732d5e0d5bb48a8704e940f78ed44
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2024-01-08 23:35:12 +0100

    Style changes in Search

--- app/app/components/search/Search.tsx
+++ app/app/components/search/Search.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { createRef, useRef, useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 export default function Search() {
+  const [focused, setFocused] = useState(false);
+
+  
+
+
   return (
     <>
-      <div className='relative'>
+      <div className='relative group'>
         <input
           className='bg-gray-100 text-black 
           md:w-[500px]
-          rounded-full px-4 py-[.6rem] outline-none border-none placeholder:font-medium placeholder:text-gray-400 w-full '
+          rounded-full px-5 text-sm py-[.6rem] outline-none border-none placeholder:font-medium placeholder:text-gray-400 w-full '
           placeholder='Search for products'
           type='text'
           id='searchProducts'
+          onFocus={() => {setFocused(true)}}
+          onBlur={() => {setFocused(false)}}
+
         />
 
-        <IoMdSearch
-          size={25}
-          className='absolute top-[9px] right-[13px] text-gray-400'
-        />
+        <button>
+          <IoMdSearch
+            tabIndex={0}
+            size={25}
+            className={`${
+              focused ? "text-gray-600" : "text-gray-400"
+            } absolute top-[9px] right-[13px]`}
+          />
+        </button>
       </div>
     </>
   );
