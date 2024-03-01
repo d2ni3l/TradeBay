fe3401d660ec7c008fa1be2ce5b95d7ade351411
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2024-03-01 13:06:58 +0100

    Next-Auth:auth options error

--- app/app/api/auth/[...nextauth]/route.ts
+++ app/app/api/auth/[...nextauth]/route.ts
@@ -1,70 +1,9 @@
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import prisma from '@/lib/prismadb'
-import bcrypt from 'bcrypt'
-import {PrismaAdapter} from '@next-auth/prisma-adapter'
-import    CredentialsProvider from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth'
+
+import { authOptions } from '../authOptions'
 
 
-export const authOptions : NextAuthOptions = {
-    adapter : PrismaAdapter(prisma),
 
-    providers:[
-        CredentialsProvider({
-
-            name: 'Credentials',
-
-            credentials: {
-                email : {label : 'email', type: 'text', placeholder: 'johndoe@gmail.com'},
-                password : {label : 'password', type: 'password' }
-            },
-
-            // authorize credentials
-            async authorize(credentials) : Promise<any>{
-            
-                if(!credentials?.email || !credentials?.password){
-            throw new Error('Invalid Data') 
-        }; 
-
-        const user = await prisma.user.findUnique({
-            where: {
-                email : credentials?.email
-            }
-        });
-
-    
-
-
-        if (!user){
-            throw new Error('Account not found')
-        }
-
-        if(!user.hashedPassword){
-            throw new Error
-        }
-
-        const passwordMatch = bcrypt.compare(credentials.password, user.hashedPassword)
-
-        if(!passwordMatch){
-            throw new Error
-        }
-
-        return user
-            }
-
-
-        })
-    ],
-
-    secret: process.env.NEXTAUTH_SECRET,
-
-    session: {
-        strategy: 'jwt'
-    },
-
-    debug : process.env.NODE_ENV === 'development'
-
-    
-}
 
 
 const handler = NextAuth(authOptions)
