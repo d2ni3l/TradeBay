c77c4eea8dbb39483fad3a4f8f7773236c2a0931
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2024-03-17 11:45:52 +0100

    added: next-cloudinary for image uploading

--- app/app/components/Inputs/ImageUpload.tsx
+++ app/app/components/Inputs/ImageUpload.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+'use client'
 
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from 'react-icons/tb'
 export default function ImageUpload() {
-  return (
-    <div>ImageUpload</div>
-  )
+  return <div></div>;
 }
