6a55aed1fd562563d1c89a5c614a7b712338392b
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2024-04-13 12:55:14 +0200

    getAllarticle.ts function with prisma

--- app/app/actions/getAllArticles.ts
+++ app/app/actions/getAllArticles.ts
@@ -1,1 +1,21 @@
 import prisma from '@/lib/prismadb'
+
+
+
+
+export default async function  (){
+
+    try{
+        const Allarticle = await prisma.article.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        }
+           
+        )
+    
+        return Allarticle;
+    }catch(error){
+  console.log(error)
+    }
+}
--- app/app/home/page.tsx
+++ app/app/home/page.tsx
@@ -2,11 +2,14 @@ import React, { Suspense } from "react";
 import Navbar from "../components/navbar/Navbar";
 import CategoryBox from "../components/categories/CategoryBox";
 import getCurrentUser from "../actions/getCurrentUser";
+import getAllArticles from "../actions/getAllArticles";
 export default async function page() {
 
   const currentUser = await getCurrentUser()
+  const allArticles = await getAllArticles()
 
 console.log(currentUser)
+console.log(allArticles)
   return (
     <>
       <Navbar />
