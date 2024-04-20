77f6ffb078c5d38eef73e48ca53d48e120773f2f
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2024-04-20 08:50:47 +0200

    Article listing card ui

--- app/app/components/articles/ListingCard.tsx
+++ app/app/components/articles/ListingCard.tsx
@@ -10,11 +10,11 @@ export default async function ({ article }: Article) {
   return (
     <>
       <div className='w-full max-w-sm relative bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-        <Badge className='absolute bg-gray-700 mt-2 mx-2'>Badge</Badge>
+        <Badge className='absolute bg-gray-700 mt-2 mx-2'>{article.category}</Badge>
 
         <div className='justify-center flex'>
           <Image
-            className='p-8 rounded-t-lg rounded-sm'
+            className='pt-8 pr-7 pl-7 pb-4 rounded-t-lg rounded-sm'
             src={article.imgSrc}
             alt='Article Image'
             width='220'
@@ -37,9 +37,9 @@ export default async function ({ article }: Article) {
             </span>
             <Button
               label='Add to cart'
-              iconClassName="text-white hover:text-black"
+              iconClassName="text-white hover:text-black group-hover:text-black"
               icon={FaCartShopping}
-              className='rounded-full flex gap-2 items-center hover:text-black hover:bg-white font-semibold   bg-black  px-3 py-2 outline-none hover:scale-[.98] outline-[1px] hover:outline-black'
+              className='rounded-full flex gap-2 items-center group hover:text-black hover:bg-white font-semibold   bg-black  px-3 py-2 outline-none hover:scale-[.98] outline-[1px] hover:outline-black'
             />
           </div>
 
@@ -86,7 +86,7 @@ export default async function ({ article }: Article) {
                 <path d='M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z' />
               </svg>
             </div>
-            <span className='bg-blue-100 text-red-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-blue-800 ms-3'>
+            <span className='bg-red-100 text-red-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-blue-800 ms-3'>
               5.0
             </span>
           </div>
