bb61398c2a3265e69c24f7f0b1e4c5555966cfb7
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2024-05-09 16:50:45 +0200

    Fixed webpack error

--- app/next.config.js
+++ app/next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: '',
-        },
-      ],
-    },
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+    ],
+  },
+
+  webpack: (config) => {
+    config.externals = [...config.externals, "bcrypt"];
+    return config;
+  },
+};
